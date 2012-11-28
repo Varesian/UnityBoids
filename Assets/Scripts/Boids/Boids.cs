7a4d407188740a2309a5795a50a77d2aef78151a
Cory Barr <cory.barr@gmail.com>
2012-11-28 00:27:51 -0800

    gave Boids more sensible defaults, removed scatterFactor arg

--- Assets/Scripts/Boids/Boids.cs
+++ Assets/Scripts/Boids/Boids.cs
@@ -6,17 +6,16 @@ public class Boids : MonoBehaviour {
 	public Boid boid;
 	public int number_of_boids = 10;  
 	
-	public float cohesionFactor = 2.0f;
-	public float repulsionFactor = 0.1f;
+	public float cohesionFactor = 0.5f;
+	public float repulsionFactor = 3.0f;
 	public float velocitySimilarityFactor = 0.01f;
 
-	public float scatterFactor = 0.0f;
-	public float speed = 0.05f;
+	public float speed = 2.0f;
 
 	public bool	maintainConstantHeight = false;
 	public bool flockHasLeader = false;
 	public bool stationaryCenter = true;
-	public bool fixYValue = true;
+	public bool fixYValue = false;
 	
 	public Vector3 center = Vector3.zero;
 	
@@ -61,7 +60,6 @@ public class Boids : MonoBehaviour {
 				boidsarray[i].velocity += getCenterAttractor(i); // boids will all fly towards center
 				boidsarray[i].velocity += getRepulsion(i); // repulse from nearby boids
 				boidsarray[i].velocity += matchVelocity(i); // match velocity
-				boidsarray[i].velocity += addScatter(i); // Scatter
 	
 				if (fixYValue) {
 					boidsarray[i].velocity.y = 0.0f;
@@ -90,12 +88,6 @@ public class Boids : MonoBehaviour {
 		
 		perceivedVelocity /= (number_of_boids - 1);
 		return perceivedVelocity * Time.deltaTime * velocitySimilarityFactor;
-	}
-	
-	Vector3 addScatter(int boidIndex)
-	{
-		// adds noise to position
-		return (new Vector3(Random.value - 0.5f, Random.value - 0.5f, Random.value - 0.5f)) * scatterFactor;		
 	}
 	
 	Vector3 getRepulsion(int boidIndex)
